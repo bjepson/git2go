077a52280537b15e9f9db19a37ea028107d70e5c
Brian Jepson <bjepson@gmail.com>
2016-12-03 16:31:03 -0500

    let's use the latest version

--- script/build-openssl-static.sh
+++ script/build-openssl-static.sh
@@ -8,7 +8,7 @@ cd $VENDORED_PATH &&
 mkdir -p build/lib &&
 
 # Switch to a stable version
-git checkout OpenSSL_1_0_2-stable &&
+git checkout OpenSSL_1_1_0-stable &&
 ./config threads no-shared --prefix=`pwd`/build -fPIC -DOPENSSL_PIC &&
 make depend &&
 make &&
