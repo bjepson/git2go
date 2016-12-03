ff018e6bf17d358ed9057802dd86addbc4fc4157
Brian Jepson <bjepson@gmail.com>
2016-12-03 16:31:21 -0500

    added missing reference to OPENSSL libs

--- script/build-libssh2-static.sh
+++ script/build-libssh2-static.sh
@@ -4,12 +4,15 @@ set -ex
 
 VENDORED_PATH=vendor/libssh2
 
+OPENSSL_ROOT_DIR=`pwd`/vendor/openssl
+
 cd $VENDORED_PATH &&
 mkdir -p install/lib &&
 mkdir -p build &&
 cd build &&
 cmake -DTHREADSAFE=ON \
       -DBUILD_CLAR=OFF \
+      -DOPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR \
       -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_C_FLAGS=-fPIC \
       -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
