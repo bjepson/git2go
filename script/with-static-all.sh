42c5a04c729b209d30112b9e0a7957ef43088857
Brian Jepson <bjepson@gmail.com>
2016-12-04 09:51:00 -0500

    Hopefully fixed duplicate libraries being linked

--- script/with-static-all.sh
+++ script/with-static-all.sh
@@ -12,7 +12,7 @@ export BUILD="$VENDOR/libgit2/build"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$LIBGIT2/build:$LIBSSH2/build/src:$OPENSSL/build/lib/pkgconfig:$PARSER"
 
 FLAGS=$(pkg-config --static --libs --cflags libcrypto openssl libssh2 libgit2) || exit 1
-export CGO_LDFLAGS="$BUILD/libgit2.a $PARSER/libhttp_parser.a $OPENSSL/libcrypto.a $OPENSSL/libssl.a -L$LIBSSH2/build/src -L$BUILD ${FLAGS}"
+export CGO_LDFLAGS="$PARSER/libhttp_parser.a -L$LIBGIT2/build -L$OPENSSL -L$LIBSSH2/build/src -L$BUILD ${FLAGS}"
 export CGO_CFLAGS="-I$LIBGIT2/include:$OPENSSL/include:$LIBSSH2/include"
 
 $@
