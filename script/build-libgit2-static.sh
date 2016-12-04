249cb9f3fe0d1432f052827d5d48c1b0c8c2e66d
Brian Jepson <bjepson@gmail.com>
2016-12-04 11:01:34 -0500

    linking libgit2 against libssh2 statically now

--- script/build-libgit2-static.sh
+++ script/build-libgit2-static.sh
@@ -3,6 +3,12 @@
 set -ex
 
 VENDORED_PATH=vendor/libgit2
+export LIBSSH2="`pwd`/vendor/libssh2"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$LIBSSH2/build/src"
+
+FLAGS=$(pkg-config --static --libs --cflags libssh2) || exit 1
+export CGO_LDFLAGS="-L$LIBSSH2/build/src ${FLAGS}"
+export CGO_CFLAGS="-I$LIBSSH2/include"
 
 cd $VENDORED_PATH &&
 mkdir -p install/lib &&
@@ -11,7 +17,7 @@ cd build &&
 cmake -DTHREADSAFE=ON \
       -DBUILD_CLAR=OFF \
       -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_C_FLAGS=-fPIC \
+      -DCMAKE_C_FLAGS="-fPIC" \
       -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
       -DCMAKE_INSTALL_PREFIX=../install \
       .. &&
